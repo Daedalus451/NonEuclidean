2a0e7d215be5d12df89fd74c5dd7c628163fe426
Daedalus <daedalus@daedala.io>
2020-07-18 23:47:14 -0400

    Change static const variables to inline constexpr
    Signed-off-by: Daedalus <daedalus@daedala.io>

--- NonEuclidean/GameHeader.h
+++ NonEuclidean/GameHeader.h
@@ -5,42 +5,42 @@
 #pragma warning(disable : 4099) // Missing PDB file
 
 // Windows
-static const char GH_TITLE[] = "NonEuclideanDemo";
-static const char GH_CLASS[] = "NED";
+inline constexpr char GH_TITLE[] = "NonEuclideanDemo";
+inline constexpr char GH_CLASS[] = "NED";
 
 // General
-static const float GH_PI = 3.141592653589793f;
-static const int GH_MAX_PORTALS = 16;
+inline constexpr float GH_PI = 3.141592653589793f;
+inline constexpr int GH_MAX_PORTALS = 16;
 
 // Graphics
-static const bool GH_START_FULLSCREEN = false;
-static const bool GH_HIDE_MOUSE = true;
-static const bool GH_USE_SKY = true;
-static const int GH_SCREEN_WIDTH = 1280;
-static const int GH_SCREEN_HEIGHT = 720;
-static const int GH_SCREEN_X = 50;
-static const int GH_SCREEN_Y = 50;
-static const float GH_FOV = 60.0f;
-static const float GH_NEAR_MIN = 1e-3f;
-static const float GH_NEAR_MAX = 1e-1f;
-static const float GH_FAR = 100.0f;
-static const int GH_FBO_SIZE = 2048;
-static const int GH_MAX_RECURSION = 4;
+inline constexpr bool GH_START_FULLSCREEN = false;
+inline constexpr bool GH_HIDE_MOUSE = true;
+inline constexpr bool GH_USE_SKY = true;
+inline constexpr int GH_SCREEN_WIDTH = 1280;
+inline constexpr int GH_SCREEN_HEIGHT = 720;
+inline constexpr int GH_SCREEN_X = 50;
+inline constexpr int GH_SCREEN_Y = 50;
+inline constexpr float GH_FOV = 60.0f;
+inline constexpr float GH_NEAR_MIN = 1e-3f;
+inline constexpr float GH_NEAR_MAX = 1e-1f;
+inline constexpr float GH_FAR = 100.0f;
+inline constexpr int GH_FBO_SIZE = 2048;
+inline constexpr int GH_MAX_RECURSION = 4;
 
 // Gameplay
-static const float GH_MOUSE_SENSITIVITY = 0.005f;
-static const float GH_MOUSE_SMOOTH = 0.5f;
-static const float GH_WALK_SPEED = 2.9f;
-static const float GH_WALK_ACCEL = 50.0f;
-static const float GH_BOB_FREQ = 8.0f;
-static const float GH_BOB_OFFS = 0.015f;
-static const float GH_BOB_DAMP = 0.04f;
-static const float GH_BOB_MIN = 0.1f;
-static const float GH_DT = 0.002f;
-static const int GH_MAX_STEPS = 30;
-static const float GH_PLAYER_HEIGHT = 1.5f;
-static const float GH_PLAYER_RADIUS = 0.2f;
-static const float GH_GRAVITY = -9.8f;
+inline constexpr float GH_MOUSE_SENSITIVITY = 0.005f;
+inline constexpr float GH_MOUSE_SMOOTH = 0.5f;
+inline constexpr float GH_WALK_SPEED = 2.9f;
+inline constexpr float GH_WALK_ACCEL = 50.0f;
+inline constexpr float GH_BOB_FREQ = 8.0f;
+inline constexpr float GH_BOB_OFFS = 0.015f;
+inline constexpr float GH_BOB_DAMP = 0.04f;
+inline constexpr float GH_BOB_MIN = 0.1f;
+inline constexpr float GH_DT = 0.002f;
+inline constexpr int GH_MAX_STEPS = 30;
+inline constexpr float GH_PLAYER_HEIGHT = 1.5f;
+inline constexpr float GH_PLAYER_RADIUS = 0.2f;
+inline constexpr float GH_GRAVITY = -9.8f;
 
 // Global variables
 class Engine;
